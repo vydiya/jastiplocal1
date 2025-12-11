03511b7d72166739bfb8a5a166c60ddeebd333a3
fatih <mfatihramdhan234@gmail.com>
2025-12-11 22:16:40 +0700

    J BESAR 2

--- app/Http/Controllers/Jastiper/DashboardJastiperController.php
+++ app/Http/Controllers/Jastiper/DashboardJastiperController.php
@@ -110,7 +110,7 @@ class DashboardJastiperController extends Controller
         }
 
 
-        return view('jastiper.dashboard.index', compact(
+        return view('Jastiper.dashboard.index', compact(
             'totalPesanan',
             'pesananSelesai',
             'ulasan',
